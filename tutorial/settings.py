1ecb5ca0e3441195b4d005b11afe160c3e18f371
hugoxia <hugoxia88@163.com>
2016-01-07 18:30:18 +0800

    new file

---
+++ tutorial/settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for tutorial project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'tutorial'
+
+SPIDER_MODULES = ['tutorial.spiders']
+NEWSPIDER_MODULE = 'tutorial.spiders'
+
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
---
+++ tutorial/spiders/joke_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+ 
+from tutorial.items import JokeItem
+
+
+class JokeSpider(scrapy.Spider):
+    name = "joke"
+    allowed_domains = ["qiushibaike.com"]
+    url_list = []
+    for i in range(1, 11):
+        page_url = "http://www.qiushibaike.com/8hr/page/" + str(i)
+        url_list.append(page_url)
+    start_urls = url_list
+ 
+    def parse(self, response):
+        # sel = scrapy.selector.Selector(response)
+        sites = response.xpath('//div[@class="article block untagged mb15"]')
+        items = []
+        for site in sites:
+            url = site.xpath('div[@class="author clearfix"]/a[@rel="nofollow"]/@href')
+            if len(url) != 0:
+                item = JokeItem()
+                item['content'] = site.xpath('div[@class="content"]/text()').extract()[0]
+                item['via_url'] = unicode("http://www.qiushibaike.com") + \
+                    site.xpath('div[@class="author clearfix"]/a[@rel="nofollow"]/@href').extract()[0]
+                item['via'] = unicode('qiushibaike')
+                items.append(item)
+
+        return items
